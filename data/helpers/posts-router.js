c4ef2463aaaf5eb7321da10a28bd6f85dbb6f8bd
ills5771 <ilyayelizarov1@gmail.com>
2019-03-20 13:55:32 -0700

    added get requests for all posts and posts by user

--- data/helpers/posts-router.js
+++ data/helpers/posts-router.js
@@ -6,10 +6,29 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const posts = await Posts.find(req.query);
+    const posts = await Posts.get(req.query);
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: "error retrieving posts" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Posts.getById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "The post information could not be retrieved."
+    });
   }
 });
 
